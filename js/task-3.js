1a57c6dd1e9bdd6c69b796bf36b0677ed4770fdb
Serhii Borysenko <borisenko37@gmail.com>
2024-05-21 23:50:53 +0200

    task 3

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const inputElem = document.querySelector('#name-input');
+const spanElem = document.querySelector('#name-output');
+console.log(inputElem);
+inputElem.addEventListener('input', onInputChange);
+function onInputChange() {
+  const value = inputElem.value;
+  if (value.length < 0) {
+    spanElem.textContent = Anonymous;
+  } else {
+    spanElem.textContent = value.trim();
+  }
+}
